08c5a60449085440be8996012470be879ad14491
tddebart <tddebart@gmail.com>
2021-09-06 11:59:51 +0200

    person

---
+++ person.py
@@ -0,0 +1,11 @@
+naam = input("Naam: ")
+adres = input("Adres: ")
+postcode = input("Postcode: ")
+woonplaats = input("Woonplaats: ")
+
+print(" ----------------------------------------------------")
+print("|  Naam      : " + naam)
+print("|  Adres     : " + adres)
+print("|  Postcode  : " + postcode)
+print("|  Woonplaats: " + woonplaats)
+print(" ----------------------------------------------------")
